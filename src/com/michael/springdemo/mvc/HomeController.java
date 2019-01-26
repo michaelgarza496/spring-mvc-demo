766667ee415d01fe6a950350e6ddac024c29a6dd
Michael Garza <michaelgarza496@gmail.com>
2019-01-26 00:10:02 -0500

    made controller and basic page to test

--- src/com/michael/springdemo/mvc/HomeController.java
+++ src/com/michael/springdemo/mvc/HomeController.java
@@ -3,6 +3,12 @@ package com.michael.springdemo.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/*
+ * Use @Controller to make the dispatcher server
+ * @RequestMapping will route the request to the given method
+ * 		just use the name of the page without the path and extension
+ */
+
 @Controller // Inherent from @Component... so component scanning :D
 public class HomeController {
 
